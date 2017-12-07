d4d12b123f23b022c80bd7befde02c516089aafe
Jared Moore <jmoore1@andrew.cmu.edu>
2017-12-07 11:52:11 -0500

    UI somewhat there

--- client/MenuMode.cpp
+++ client/MenuMode.cpp
@@ -7,11 +7,14 @@
 #include "Scene.hpp"
 
 #include <cmath>
+#include <functional>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 //---------- resources ------------
 Load<MeshBuffer> menuMeshes(LoadTagInit, []() { return new MeshBuffer("menu.p"); });
+
+std::function<void(const std::string&, float, float, float)> drawWord;
 
 // Attrib locations in menu_program:
 GLint menuProgramPosition = -1;
@@ -38,7 +41,6 @@ Load<GLProgram> menuProgram(LoadTagInit, []() {
 	menuProgramPosition = (*ret)("Position");
 	menuProgramMVP = (*ret)["mvp"];
 	menuProgramColor = (*ret)["color"];
-
 	return ret;
 });
 
@@ -52,23 +54,79 @@ Load<GLVertexArray> menuBinding(LoadTagDefault, []() {
 });
 
 //----------------------
-MenuMode::MenuMode() {}
+MenuMode::MenuMode() {
+	drawWord = [](const std::string& word, float x, float y, float fontSize) {
+		glUseProgram(menuProgram->program);
+		glBindVertexArray(menuBinding->array);
 
-bool MenuMode::handle_event(SDL_Event const& e, glm::uvec2 const& window_size) {
-	if (e.type == SDL_KEYDOWN) {
-		if (e.key.keysym.sym == SDLK_ESCAPE) {
-			Mode::set_current(nullptr);
-			return true;
-		} else if (e.key.keysym.sym == SDLK_UP) {
-			menuButtons.onPrev();
-			return true;
-		} else if (e.key.keysym.sym == SDLK_DOWN) {
-			menuButtons.onNext();
-			return true;
-		} else if (e.key.keysym.sym == SDLK_RETURN || e.key.keysym.sym == SDLK_SPACE) {
-			menuButtons.onKey();
-			return true;
+		auto width = [](char a) {
+			if (a == 'I')
+				return 1.0f;
+			else if (a == 'L')
+				return 2.0f;
+			else if (a == 'M' || a == 'W')
+				return 4.0f;
+			else
+				return 3.0f;
+		};
+		auto spacing = [](char a, char b) { return 1.0f; };
+
+		float total_width = 0.0f;
+		for (uint32_t i = 0; i < word.size(); ++i) {
+			if (i > 0)
+				total_width += spacing(word[i - 1], word[i]);
+			total_width += width(word[i]) * 0.75f;
 		}
+
+		static const float height = 1.0f;
+		y += -0.5f * 0.1 * height;									// center y
+		x += -0.5f * total_width * 0.1f * 0.3333f;	// center x
+		for (uint32_t i = 0; i < word.size(); ++i) {
+			if (i > 0) {
+				x += spacing(word[i], word[i - 1]) * 0.1f * 0.3333f;
+			}
+
+			if (word[i] != ' ') {
+				float s = fontSize * 0.1f * (1.0f / 3.0f);
+				glm::mat4 mvp = glm::mat4(glm::vec4(s, 0.0f, 0.0f, 0.0f), glm::vec4(0.0f, s, 0.0f, 0.0f),
+																	glm::vec4(0.0f, 0.0f, 1.0f, 0.0f), glm::vec4(x, y, 0.0f, 1.0f));
+				glUniformMatrix4fv(menuProgramMVP, 1, GL_FALSE, glm::value_ptr(mvp));
+				glUniform3f(menuProgramColor, 1.0f, 1.0f, 1.0f);
+
+				MeshBuffer::Mesh const& mesh = menuMeshes->lookup(word.substr(i, 1));
+				glDrawArrays(GL_TRIANGLES, mesh.start, mesh.count);
+			}
+
+			x += width(word[i]) * 0.75f * 0.1f * 0.3333f;
+		}
+	};
+}
+
+bool MenuMode::handle_event(SDL_Event const& event, glm::uvec2 const& window_size) {
+	static glm::vec2 mouse;
+
+	if (event.type == SDL_MOUSEMOTION) {
+		mouse.x = (event.motion.x + 0.5f) / window_size.x * 2.0f - 1.0f;
+		mouse.y = (event.motion.y + 0.5f) / window_size.y * -2.0f + 1.0f;
+
+		menuButtons.checkMouseHover(mouse.x, mouse.y);
+	}
+
+	if (event.type == SDL_MOUSEBUTTONDOWN) {
+		menuButtons.onClick();
+	}
+
+	if (event.type == SDL_KEYDOWN) {
+		if (event.key.keysym.sym == SDLK_LSHIFT || event.key.keysym.sym == SDLK_RSHIFT) {
+			menuButtons.onKey();
+		} else if (event.key.keysym.sym == SDLK_a || event.key.keysym.sym == SDLK_w || event.key.keysym.sym == SDLK_UP ||
+							 event.key.keysym.sym == SDLK_LEFT) {
+			menuButtons.onPrev();
+		} else if (event.key.keysym.sym == SDLK_d || event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_s ||
+							 event.key.keysym.sym == SDLK_DOWN) {
+			menuButtons.onNext();
+		}
+		return true;
 	}
 	return false;
 }
@@ -79,12 +137,12 @@ void MenuMode::update(float elapsed) {
 }
 
 void MenuMode::draw(glm::uvec2 const& drawable_size) {
-	float aspect = drawable_size.x / float(drawable_size.y);
+	/*float aspect = drawable_size.x / float(drawable_size.y);
 	// scale factors such that a rectangle of aspect 'aspect' and height '1.0' fills the window:
 	glm::vec2 scale = glm::vec2(1.0f / aspect, 1.0f);
 	glm::mat4 projection = glm::mat4(glm::vec4(scale.x, 0.0f, 0.0f, 0.0f), glm::vec4(0.0f, scale.y, 0.0f, 0.0f),
 																	 glm::vec4(0.0f, 0.0f, 1.0f, 0.0f), glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
-
+*/
 	float total_height = 0.0f;
 	for (auto const& choice : choices) {
 		total_height += choice.height + 2.0f * choice.padding;
@@ -92,69 +150,5 @@ void MenuMode::draw(glm::uvec2 const& drawable_size) {
 
 	menuButtons.draw();
 
-	glUseProgram(menuProgram->program);
-	glBindVertexArray(menuBinding->array);
-
-	// character width and spacing helpers:
-	// (...in terms of the menu font's default 3-unit height)
-	auto width = [](char a) {
-		if (a == 'I')
-			return 1.0f;
-		else if (a == 'L')
-			return 2.0f;
-		else if (a == 'M' || a == 'W')
-			return 4.0f;
-		else
-			return 3.0f;
-	};
-	auto spacing = [](char a, char b) { return 1.0f; };
-
-	float select_bounce = std::abs(std::sin(bounce * 3.1515926f * 2.0f));
-
-	float y = 0.5f * total_height;
-	for (auto const& choice : choices) {
-		y -= choice.padding;
-		y -= choice.height;
-
-		bool is_selected = (&choice - &choices[0] == selected);
-		std::string label = choice.label;
-
-		if (is_selected) {
-			label = "*" + label + "*";
-		}
-
-		float total_width = 0.0f;
-		for (uint32_t i = 0; i < label.size(); ++i) {
-			if (i > 0)
-				total_width += spacing(label[i - 1], label[i]);
-			total_width += width(label[i]);
-		}
-		if (is_selected) {
-			total_width += 2.0f * select_bounce;
-		}
-
-		float x = -0.5f * total_width;
-		for (uint32_t i = 0; i < label.size(); ++i) {
-			if (i > 0)
-				x += spacing(label[i - 1], label[i]);
-			if (is_selected && (i == 1 || i + 1 == label.size())) {
-				x += select_bounce;
-			}
-
-			if (label[i] != ' ') {
-				float s = choice.height * (1.0f / 3.0f);
-				glm::mat4 mvp = projection * glm::mat4(glm::vec4(s, 0.0f, 0.0f, 0.0f), glm::vec4(0.0f, s, 0.0f, 0.0f),
-																							 glm::vec4(0.0f, 0.0f, 1.0f, 0.0f), glm::vec4(s * x, y, 0.0f, 1.0f));
-				glUniformMatrix4fv(menuProgramMVP, 1, GL_FALSE, glm::value_ptr(mvp));
-				glUniform3f(menuProgramColor, 1.0f, 1.0f, 1.0f);
-
-				MeshBuffer::Mesh const& mesh = menuMeshes->lookup(label.substr(i, 1));
-				glDrawArrays(GL_TRIANGLES, mesh.start, mesh.count);
-			}
-
-			x += width(label[i]);
-		}
-
-		y -= choice.padding;
-	}
+	drawWord("ODD ONE OUT", 0.0f, 0.6f, 1.0f);
 }
--- client/main.cpp
+++ client/main.cpp
@@ -2,6 +2,7 @@
 #include "MenuMode.hpp"
 #include "Socket.hpp"
 #include "StagingMode.hpp"
+#include "load_save_png.hpp"
 
 #include "GL.hpp"
 #include "Load.hpp"
@@ -14,6 +15,7 @@
 
 #include <chrono>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
@@ -27,6 +29,22 @@ std::string remoteAddress;
 std::string port;
 unsigned crowdSize;
 
+GLuint tex;
+GLuint vao;
+GLuint buffer;
+
+// shader program:
+GLuint program = 0;
+GLuint program_Position = 0;
+GLuint program_TexCoord = 0;
+GLuint program_Color = 0;
+GLuint program_mvp = 0;
+GLuint program_tex = 0;
+
+/*
+static GLuint compile_shader(GLenum type, std::string const& source);
+static GLuint link_program(GLuint vertex_shader, GLuint fragment_shader);
+*/
 int main(int argc, char** argv) {
 	// Sound::init(argc, argv);
 
@@ -96,15 +114,113 @@ int main(int argc, char** argv) {
 
 	//------------ load all assets -----------
 	call_load_functions();
+	/*
+	{	// compile shader program:
+		GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER,
+																					"#version 330\n"
+																					"uniform mat4 mvp;\n"
+																					"in vec4 Position;\n"
+																					"in vec2 TexCoord;\n"
+																					"in vec4 Color;\n"
+																					"out vec2 texCoord;\n"
+																					"out vec4 color;\n"
+																					"void main() {\n"
+																					"	gl_Position = mvp * Position;\n"
+																					"	color = Color;\n"
+																					"	texCoord = TexCoord;\n"
+																					"}\n");
+
+		GLuint fragment_shader = compile_shader(GL_FRAGMENT_SHADER,
+																						"#version 330\n"
+																						"uniform sampler2D tex;\n"
+																						"in vec4 color;\n"
+																						"in vec2 texCoord;\n"
+																						"out vec4 fragColor;\n"
+																						"void main() {\n"
+																						"	fragColor = texture(tex, texCoord) * color;\n"
+																						"}\n");
+
+		program = link_program(fragment_shader, vertex_shader);
+
+		// look up attribute locations:
+		program_Position = glGetAttribLocation(program, "Position");
+		if (program_Position == -1U)
+			throw std::runtime_error("no attribute named Position");
+		program_TexCoord = glGetAttribLocation(program, "TexCoord");
+		if (program_TexCoord == -1U)
+			throw std::runtime_error("no attribute named TexCoord");
+		program_Color = glGetAttribLocation(program, "Color");
+		if (program_Color == -1U)
+			throw std::runtime_error("no attribute named Color");
+
+		// look up uniform locations:
+		program_mvp = glGetUniformLocation(program, "mvp");
+		if (program_mvp == -1U)
+			throw std::runtime_error("no uniform named mvp");
+		program_tex = glGetUniformLocation(program, "tex");
+		if (program_tex == -1U)
+			throw std::runtime_error("no uniform named tex");
+	}
+
+	// vertex buffer:
+	buffer = 0;
+	{	// create vertex buffer
+		glGenBuffers(1, &buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	}
+
+	// vertex array object:
+	vao = 0;
+	{	// create vao and set up binding:
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		glVertexAttribPointer(program_Position, 2, GL_FLOAT, GL_FALSE,
+													sizeof(glm::vec2) + sizeof(glm::vec2) + sizeof(glm::u8vec4), (GLbyte*)0);
+		glVertexAttribPointer(program_TexCoord, 2, GL_FLOAT, GL_FALSE,
+													sizeof(glm::vec2) + sizeof(glm::vec2) + sizeof(glm::u8vec4), (GLbyte*)0 + sizeof(glm::vec2));
+		glVertexAttribPointer(program_Color, 4, GL_UNSIGNED_BYTE, GL_TRUE,
+													sizeof(glm::vec2) + sizeof(glm::vec2) + sizeof(glm::u8vec4),
+													(GLbyte*)0 + sizeof(glm::vec2) + sizeof(glm::vec2));
+		glEnableVertexAttribArray(program_Position);
+		glEnableVertexAttribArray(program_TexCoord);
+		glEnableVertexAttribArray(program_Color);
+	}
+
+	// load png from game 1 code
+	tex = 0;
+	glm::uvec2 tex_size = glm::uvec2(0, 0);
+	{	// load texture 'tex':
+		std::vector<uint32_t> data;
+		if (!load_png("ui.png", &tex_size.x, &tex_size.y, &data, LowerLeftOrigin)) {
+			std::cerr << "Failed to load texture." << std::endl;
+			exit(1);
+		} else {
+			std::cout << "loaded ui.png with size " << tex_size.x << " " << tex_size.y << " " << data[0] << " " << data[1]
+								<< std::endl;
+		}
+		// create a texture object:
+		glGenTextures(1, &tex);
+		// bind texture object to GL_TEXTURE_2D:
+		glBindTexture(GL_TEXTURE_2D, tex);
+		// upload texture data from data:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_size.x, tex_size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, &data[0]);
+		// set texture sampling parameters:
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	}*/
 
 	//------------ set up modes -----------
 	std::shared_ptr<GameMode> game = std::make_shared<GameMode>();
 	std::shared_ptr<StagingMode> staging = std::make_shared<StagingMode>();
 	std::shared_ptr<MenuMode> menu = std::make_shared<MenuMode>();
 
+	glm::vec3 color = {0.5f, 1.0f, 0.0f};
+
 	Button* btn;
-	btn = menu->menuButtons.add("playLocal", {0.0f, 1.0f, 0.0f});
-	btn->isEnabled = []() { return true; };
+	btn = menu->menuButtons.add("PLAY LOCAL", color);
+	btn->isEnabled = []() { return false; };
 	btn->onFire = [&]() {
 		std::thread([]() { Server server; }).detach();
 
@@ -115,36 +231,25 @@ int main(int argc, char** argv) {
 		Mode::set_current(staging);
 	};
 
-	menu->menuButtons.layoutVertical(0.092f, 1.0f);
-
-	menu->choices.emplace_back("ODD ONE OUT");
-	menu->choices.emplace_back("PLAY LOCAL", [&](MenuMode::Choice&) {
-		std::thread([]() { Server server; }).detach();
-
-		// wait for server to start accepting connections
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-		staging->reset(true);
-		Mode::set_current(staging);
-	});
-	menu->choices.emplace_back("PLAY ONLINE", [&](MenuMode::Choice&) {
+	btn = menu->menuButtons.add("PLAY ONLINE", color);
+	btn->isEnabled = []() { return true; };
+	btn->onFire = [&]() {
 		staging->reset(false);
 		Mode::set_current(staging);
-	});
-	menu->choices.emplace_back("QUIT", [&](MenuMode::Choice&) { Mode::set_current(nullptr); });
-	menu->selected = 1;
+	};
+
+	btn = menu->menuButtons.add("QUIT", color);
+	btn->isEnabled = []() { return true; };
+	btn->onFire = [&]() { Mode::set_current(nullptr); };
+
+	menu->menuButtons.layoutVertical(0.092f, 0.65f, -0.2f);
 
 	staging->enterGame = [&](Socket* sock, std::unique_ptr<StagingMode::StagingState> stagingState) {
 		game->sock = sock;
 		game->reset(std::move(stagingState));
 		Mode::set_current(game);
 	};
-	staging->showMenu = [&]() {
-		menu->choices[0].label = "ODD ONE OUT";
-		menu->choices[1].label = "MULTIPLAYER";
-		menu->selected = 2;
-		Mode::set_current(menu);
-	};
+	staging->showMenu = [&]() {};
 
 	std::string startingMode = argc > 4 ? argv[4] : "";
 	std::string position = argc > 5 ? argv[5] : "";
@@ -163,11 +268,11 @@ int main(int argc, char** argv) {
 		std::thread setup([&]() {
 			std::this_thread::sleep_for(std::chrono::milliseconds(200));
 			if (position == "robber") {
-				staging->robberBtn.onFire();
+				// staging->robberBtn.onFire();
 				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				staging->startBtn.onFire();
+				// staging->startBtn.onFire();
 			} else {
-				staging->copBtn.onFire();
+				// staging->copBtn.onFire();
 			}
 		});
 		setup.detach();
@@ -245,3 +350,46 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
+/*
+static GLuint compile_shader(GLenum type, std::string const& source) {
+	GLuint shader = glCreateShader(type);
+	GLchar const* str = source.c_str();
+	GLint length = source.size();
+	glShaderSource(shader, 1, &str, &length);
+	glCompileShader(shader);
+	GLint compile_status = GL_FALSE;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+	if (compile_status != GL_TRUE) {
+		std::cerr << "Failed to compile shader." << std::endl;
+		GLint info_log_length = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_log_length);
+		std::vector<GLchar> info_log(info_log_length, 0);
+		GLsizei length = 0;
+		glGetShaderInfoLog(shader, info_log.size(), &length, &info_log[0]);
+		std::cerr << "Info log: " << std::string(info_log.begin(), info_log.begin() + length);
+		glDeleteShader(shader);
+		throw std::runtime_error("Failed to compile shader.");
+	}
+	return shader;
+}
+
+static GLuint link_program(GLuint fragment_shader, GLuint vertex_shader) {
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	GLint link_status = GL_FALSE;
+	glGetProgramiv(program, GL_LINK_STATUS, &link_status);
+	if (link_status != GL_TRUE) {
+		std::cerr << "Failed to link shader program." << std::endl;
+		GLint info_log_length = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_log_length);
+		std::vector<GLchar> info_log(info_log_length, 0);
+		GLsizei length = 0;
+		glGetProgramInfoLog(program, info_log.size(), &length, &info_log[0]);
+		std::cerr << "Info log: " << std::string(info_log.begin(), info_log.begin() + length);
+		throw std::runtime_error("Failed to link program");
+	}
+	return program;
+}
+*/
